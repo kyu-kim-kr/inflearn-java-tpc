3fd2c63b69f898e3ecbbb6af84cab544a41228af
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-01-15 20:44:43 +1300

    프라이빗 생성자

---
+++ src/TPC31.java
@@ -0,0 +1,15 @@
+import kr.tpc.Inflearn;
+
+public class TPC31 {
+    public static void main(String[] args) {
+        Inflearn.tpc();
+//        inf.java()
+        Inflearn.java();
+        // ->
+
+        // Java API 생성자 private
+//        System sys = new System();
+//        Math math = new Math();
+
+    }
+}
