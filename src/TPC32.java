efbd14bfe5569db976b129fead930a2b873f104d
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-01-25 12:16:40 +1300

    배열 API 만들기

--- src/TPC32.java
+++ src/TPC32.java
@@ -10,5 +10,15 @@ public class TPC32 {
 
         conn = new JavaMySQL();
         conn.getConnection("url", "mysqlKyu", "12345");
+
+        String str1 = "asd";
+        String str2 = "asd";
+        System.out.println(str1.equals(str2));
+        System.out.println(str1 == str2);
+
+        String str3 = new String("asd");
+        String str4 = new String("asd");
+        System.out.println(str3.equals(str4));
+        System.out.println(str3 == str4);
     }
 }
